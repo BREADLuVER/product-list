2d099c8801cc86f7c93451aa85a7df24178dbcff
BREADLuVER <ry2050@nyu.edu>
2025-05-09 16:12:28 -0400

    offline mod

--- product-ls/src/lib/react-query.ts
+++ product-ls/src/lib/react-query.ts
@@ -1,4 +1,7 @@
 import { QueryClient } from '@tanstack/react-query'
+import { persistQueryClient } from '@tanstack/react-query-persist-client'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import localforage from 'localforage'
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -7,4 +10,14 @@ export const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
+})
+
+const persister = createAsyncStoragePersister({
+  storage: localforage,
+})
+
+persistQueryClient({
+  queryClient,
+  persister,
+  maxAge: 1000 * 60 * 60 * 24,
 })
